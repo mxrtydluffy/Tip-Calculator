074eb013e33585efda73faaaadd60390b83f2d92
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-02-17 00:11:45 -0800

    [FINISHED]

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -1,48 +1,16 @@
 import react, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/calculator';
 
 function App() {
-  const [name, setName] = useState('')
- const [email, setEmail] = useState('')
 
   return (
-    <div>
-
-      <form>
-        <label>Bill Total</label>
-        <input 
-          type="text"
-          placeholder='0.00'
-          value={name}
-          onChange={(e) => setName(e.target.value )}
-        />
-
-        <label>Tip Amount</label>
-        <input 
-          type="text"
-          placeholder='0.00'
-          value={email}
-          onChange={(e) => setEmail(e.target.value )}
-        />
-      </form>
-      <div className='summary'>
-        <div className='seperate'>
-          <label># of people</label>
-          <div className='split-control'>
-            <button>-</button>
-            <span>1</span>
-            <button>+</button>
-          </div>
-        </div>
-        <div className='result'>
-          <label>Split Total</label>
-          <span>100.00</span>
-        </div>
-      </div>
-
+    <div className='App'>
+      <header className='App-header'>
+        <Calculator />
+      </header>
     </div>
-    
   );
 }
 
--- tip-calculator/src/components/calculator.js
+++ tip-calculator/src/components/calculator.js
@@ -1,63 +1,63 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import "./calculator.css";
 
 function Calculator() {
 
-    const [bill,setBill] = useState('0.00');
-    const [tipPercentage,setTip] = useState('15%');
-    const [split,setSplit] = useState(1);
+  const [tip,setTip] = useState('10%');
+  const [bill,setBill] = useState('');
+  const [seperate,setSplit] = useState(1);
+  const [splitTotal,setSplitTotal] = useState(0);
 
-    // For percantage sign
-    // function handleTipChange(e) {
-    //     let value = e.target.value.replace('%', '');
-    //     if (value.indexOf(value = '%') === -1) {
-    //         value = value + '%';
-    //     }
-    //     setTip(value);
-    // }
-    const tip = (bill * tipPercentage / 100) / split;
-    const total = (bill * (1 + tipPercentage / 100)) / split;
-
-    const changeBill = (e) => {
-        setBill(e.target.value);
+  {/* Manages tip */}
+  function handleTipChange(e) {
+    let value = e.target.value.replace('%','');
+    if (value.indexOf('%') === -1) {
+      value = value+'%';
     }
+    setTip(value);
+  }
 
-    const tipAmount = (e) => {
-        setTip(e.target.value);
-    }
+  {/* Event Listener loking for value */}
+  function handleBillChange(e) {
+    setBill(e.target.value);
+  }
 
-    const peopleAmount = (e) => {
-        setSplit(e.target.value);
-    }
+  function splitMinus() {
+    setSplit(oldValue => Math.max(oldValue - 1, 1));
+  }
 
-    const handleClick = (e) => {
-        if (e.target.name === "decrease") {
-            setTip(tipPercentage - 1);
-        } else  if (e.target.name === "increase") {
-            setTip(tipPercentage + 1);
-        } else if (e.target.name === ""){
-            setSplit(split - 1)
-        } else if (e.target.name === ""){
-            setSplit(split +1);
-        }
-    }
+  function splitPlus() {
+    setSplit(oldValue => oldValue + 1);
+  }
+
+  {/* Calculates the percantages and results */}
+  function calculate() {
+    const percentage = 1 + parseInt(tip.replace('%','')) / 100;
+    const result = (bill * percentage / seperate).toFixed(2);
+    setSplitTotal(result);
+  }
+
+  useEffect(() => {
+    calculate();
+  }, [bill,tip,seperate])
 
     return(
         <div>
+        <h1>Marty's Tip Calculator</h1>
         {/* First Form Bill Total */}
       <form>
-        <label>Bill Total</label>
+        <label>Bill</label>
         <input 
-          type="number"
-          placeholder='0.00'
+          type="text"
+          placeholder={'0.00'}
           value={bill}
-          onChange={changeBill}
+          onChange={handleBillChange}
         />
 
         {/* Second form Tip Amount */}
         <label>Tip Amount</label>
         <input 
-          type="number"
+          type="text"
           placeholder={'0.00'}
           value={tip}
           onChange={handleTipChange}
@@ -69,18 +69,18 @@ function Calculator() {
         <div className='seperate'>
           <label># of people</label>
           <div className='split-control'>
-            <button name="decrease">-</button>
-            <span>{split}</span>
-            <button name='increase'>+</button>
+            <button onClick={splitMinus}>-</button>
+            <span>{seperate}</span>
+            <button onClick={splitPlus}>+</button>
           </div>
         </div>
         <div className='result'>
-          <label>Split Total</label>
-          { split}
+          <label>Total</label>
+          <span>{splitTotal}</span>
         </div>
       </div>
     </div>
-    )
+    );
 }
 
 export default Calculator;
